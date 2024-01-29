023d5e75fcba344dc354971ede0a31d849717a0f
Henrik Aleksanyan <healeksa@c2r11s6.42yerevan.am>
2024-01-29 11:57:15 +0400

    added new test & fixed strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: healeksa <healeksa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 17:27:35 by healeksa          #+#    #+#             */
-/*   Updated: 2024/01/28 18:44:31 by healeksa         ###   ########.fr       */
+/*   Updated: 2024/01/29 11:54:56 by healeksa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,27 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*res;
+	size_t	i;
+	size_t	j;
 
 	if (!s1 || !s2)
 		return (NULL);
-	res = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	res = (char *)malloc(sizeof(*s1) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!res)
 		return (NULL);
-	ft_strlcpy(res, s1, ft_strlen(s1) + 1);
-	ft_strlcpy(res + ft_strlen(s1), s2, ft_strlen(s2) + 1);
-	res[ft_strlen(s1) + ft_strlen(s2) + 1] = '\0';
+	i = 0;
+	j = 0;
+	while (s1[i])
+	{
+		res[j++] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (s2[i])
+	{
+		res[j++] = s2[i];
+		i++;
+	}
+	res[j] = 0;
 	return (res);
 }
